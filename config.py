3874026ed3468b7e7c41d40b422a30359ab6f1fa
momenai <momen.mohammed.ai@gmail.com>
2023-07-17 02:22:19 +0300

    last update

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import openai
 # Set your environment variables
 # openai.api_key = "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"
 
-os.environ["_BARD_API_KEY"] = "YQjjdUJLMYHsCO8EclcYXFM2MuT7u77hzF7XvjaPEfPsaONAk2qouzGJY_6P7KjQLLDq0A."
-openai.api_key = "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"
+os.environ["_BARD_API_KEY"] = "."
+openai.api_key = "sk-"
